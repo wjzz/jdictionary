c90f8f54e791e2682cf3c26d7a67e7bd4f8eca8a
wjzz <wjedynak@gmail.com>
2010-07-22 01:53:42 +0200

    Some minor changes

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,13 +1,20 @@
 class WordsController < ApplicationController
   active_scaffold :word do |config|
     # always show the new word form (useful while configuring the form)
-    config.list.always_show_create = true
-    
+    config.list.always_show_create = false
+    config.list.always_show_search = true
+
+ #   config.actions.swap :search, :field_search
+
     # The order in the form
     config.columns = [:japanese, :reading, :definition, :category, :tag_list, :comments]
 
     # Fields listed in the main list
-    config.list.columns = [:japanese, :reading, :definition, :tag_list]
+    config.list.columns = [:japanese, :reading, :comments, :tag_list]
+
+    # Virtual ields used in searching
+    #config.search.columns = [:japanese, :reading, :definition, :tag_list, :comments]
+#    config.columns[:tag_list].search_ui = :string
 
     ## Field configurations
     # Definition textarea size
@@ -19,6 +26,9 @@ class WordsController < ApplicationController
     # Category options
     config.columns[:category].form_ui = :select
     config.columns[:category].options = { :options => (Word::CATEGORY).map {|l| [l,l]} }
+    
+    #config.columns[:reading].inplace_edit = :ajax
+    #config.search.live = true
     
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -4,4 +4,8 @@ class Word < ActiveRecord::Base
   CATEGORY = %w(noun u_verb ru_verb na_adjective i_adjective particle other)
 
   validates_inclusion_of :category, :in => CATEGORY
+
+  def to_label
+    "Word: #{japanese}"
+  end
 end
